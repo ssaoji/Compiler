0c3ba54d28208a90e24685ea6202efc46d2b8780
Sunchet Saoji <ssaoji@purdue.edu>
2019-11-18 19:17:00 -0500

    file reading

--- src/Compiler.java
+++ src/Compiler.java
@@ -1,5 +1,20 @@
+import java.io.*;
+
 public class Compiler {
     public static void main (String[] args){
         System.out.println("Fuck this project this stupid ass project");
+
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader(filename)); //TODO: change filename
+            String line = reader.readLine();
+            while (line != null) {
+                parseLine(line); //TODO: implement parseLine
+                line = reader.readLine();
+            }
+            }catch(Exception e){
+                System.out.println("Exception thrown when reading file");
+            }
+        }
     }
 }
