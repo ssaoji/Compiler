2d3a1617648c1e36accb9ebbc1da2b03dfc5a777
chrisoflaherty <chrisoflaherty@me.com>
2019-12-05 11:24:57 -0500

    finished

--- src/Compiler.java
+++ src/Compiler.java
@@ -7,23 +7,117 @@ import java.util.*;
 public class Compiler {
 
     public static void main (String[] args) throws IOException {
-        OutputStream os = new FileOutputStream("Basics2.bin");
+        String filename = "Simple_Variables.txt";
+        OutputStream os = new FileOutputStream("output.bin");
         Map<String, Integer> symbol_table = new HashMap<>();
+        Map<String, Integer> labels = new HashMap<>();
         int decl_offset = 0;
         ArrayList<Integer> stack = new ArrayList<Integer>();
+        int pc = 0;
         // Starts writing the bytes in it
         try {
-            BufferedReader reader = new BufferedReader(new FileReader("Basics.txt"));
+            BufferedReader reader = new BufferedReader(new FileReader(filename));
             String line = reader.readLine();
-
             while (line != null) {
-                //System.out.print(line);
                 String[ ] tokens = line.split(" ");
-                //System.out.println(tokens[0]);
                 line = reader.readLine();
                 if(tokens[0].equals("decl")) {
                     //do decl stuff
                     symbol_table.put(tokens[1], decl_offset++);
+                    pc += 5;
+                }
+                else if(tokens[0].equals("lab")){
+                    //do lab stuff
+                    labels.put(tokens[1], pc);
+                }
+                else if(tokens[0].equals("subr")){
+                    //do subr stuff
+                    pc += 17;
+                }
+                else if(tokens[0].equals("printi")){
+                    //do printi stuff
+                    pc += 6;
+                }
+                else if(tokens[0].equals("printv")){
+                    //do printv stuff
+                    pc += 7;
+                }
+                else if(tokens[0].equals("jmp")){
+                    //do jmp stuff
+                    pc += 6;
+                }
+                else if(tokens[0].equals("jmpc")){
+                    //do jmpc stuff
+                    pc += 6;
+                } else if (tokens[0].equals("cmpe")) {
+                    //do cmpe stuff
+                    pc += 1;
+                }
+                else if(tokens[0].equals("cmplt")){
+                    //do cmplt stuff
+                    pc += 1;
+                }
+                else if(tokens[0].equals("cmpgt")){
+                    //do cmpgt stuff
+                    pc += 1;
+                }
+                else if(tokens[0].equals("pushi")){
+                    //do pushi stuff
+                    pc += 5;
+                }
+                else if(tokens[0].equals("pushv")){
+                    //do pushv stuff
+                    pc += 6;
+                }
+                else if(tokens[0].equals("popm")){
+                    //do popm stuff
+                    pc += 6;
+                }
+                else if(tokens[0].equals("popv")){
+                    //do popv stuff
+                    pc += 6;
+                }
+                else if(tokens[0].equals("peek")){
+                    //do peek stuff
+                    pc += 11;
+                }
+                else if(tokens[0].equals("poke")){
+                    //do poke stuff
+                    pc += 11;
+                }
+                else if(tokens[0].equals("swp")){
+                    //do swp stuff
+                    pc += 1;
+                }
+                else if(tokens[0].equals("add")){
+                    //do add stuff
+                    pc += 1;
+                }
+                else if(tokens[0].equals("sub")){
+                    //do sub stuff
+                    pc += 1;
+                }
+                else if(tokens[0].equals("mul")){
+                    //do mul stuff
+                    pc += 1;
+                }
+                else if(tokens[0].equals("div")){
+                    //do div stuff
+                    pc += 1;
+                }
+                else if(tokens[0].equals("ret")){
+                    pc += 8;
+                }
+            }
+            reader.close();
+            reader = new BufferedReader(new FileReader(filename));
+            line = reader.readLine();
+
+            while (line != null) {
+                String[ ] tokens = line.split(" ");
+                line = reader.readLine();
+                if(tokens[0].equals("decl")) {
+                    //do decl stuff
                     os.write((byte)(70));
                     os.write(toBytes(0));
                 }
@@ -56,9 +150,15 @@ public class Compiler {
                 }
                 else if(tokens[0].equals("jmp")){
                     //do jmp stuff
+                    os.write((byte)(70));
+                    os.write(toBytes(labels.get(tokens[1])));
+                    os.write((byte)(36));
                 }
                 else if(tokens[0].equals("jmpc")){
                     //do jmpc stuff
+                    os.write((byte)(70));
+                    os.write(toBytes(labels.get(tokens[1])));
+                    os.write((byte)(40));
                 } else if (tokens[0].equals("cmpe")) {
                     //do cmpe stuff
                     os.write((byte)(132));
@@ -144,11 +244,11 @@ public class Compiler {
             System.out.println("Exception thrown when reading file");
         }
         os.close();
-        /*
-        byte[] bytes = {(byte)0, (byte)0, (byte)15, (byte)164};
-        int result = fromByteArray(bytes);
-        System.out.println(result);
-         */
+
+        //byte[] bytes = {(byte)0, (byte)0, (byte)1, (byte)44};
+        //int result = fromByteArray(bytes);
+        //System.out.println(result);
+
 
 
     }
@@ -161,13 +261,14 @@ public class Compiler {
         result[0] = (byte) (i /*>> 0*/);
         return result;
     }
-    /*
+/*
     public static int fromByteArray(byte[] bytes) {
         return ((bytes[0] & 0xFF) << 24) |
                 ((bytes[1] & 0xFF) << 16) |
                 ((bytes[2] & 0xFF) << 8 ) |
                 ((bytes[3] & 0xFF) << 0 );
     }
-     */
+ */
+
 
 }
