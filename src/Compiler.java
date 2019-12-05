5bcef1c03df6167db5a4d3d08131f11775a03b37
chrisoflaherty <chrisoflaherty@me.com>
2019-12-04 23:15:13 -0500

    toBytes made

--- src/Compiler.java
+++ src/Compiler.java
@@ -7,29 +7,23 @@ import java.util.*;
 public class Compiler {
 
     public static void main (String[] args) throws IOException {
-        BufferedReader reader = null;
-        OutputStream os = new FileOutputStream("Basics.bin");
-        Map<String, Integer> symbol_table = null;
+        OutputStream os = new FileOutputStream("Basics2.bin");
+        Map<String, Integer> symbol_table = new HashMap<>();
         int decl_offset = 0;
-
+        ArrayList<Integer> stack = new ArrayList<Integer>();
         // Starts writing the bytes in it
-
-
-        byte[] result = toBytes(70);
-
         try {
-            reader = new BufferedReader(new FileReader("basics.txt"));
+            BufferedReader reader = new BufferedReader(new FileReader("Basics.txt"));
             String line = reader.readLine();
 
             while (line != null) {
+                //System.out.print(line);
                 String[ ] tokens = line.split(" ");
-                System.out.println(tokens[0]);
+                //System.out.println(tokens[0]);
                 line = reader.readLine();
-
-
                 if(tokens[0].equals("decl")) {
                     //do decl stuff
-                    symbol_table.put("main"+tokens[1], decl_offset++);
+                    symbol_table.put(tokens[1], decl_offset++);
                     os.write((byte)(70));
                     os.write(toBytes(0));
                 }
@@ -38,7 +32,6 @@ public class Compiler {
                 }
                 else if(tokens[0].equals("subr")){
                     //do subr stuff
-                    ///writer.write("hi");
                     os.write((byte)(70));
                     os.write(toBytes(16));
                     os.write((byte)(70));
@@ -47,18 +40,19 @@ public class Compiler {
                     os.write(toBytes(1));
                     os.write((byte)(44));
                     os.write((byte)(0));
-
                 }
                 else if(tokens[0].equals("printi")){
                     //do printi stuff
-                    //writer.write(toBinaryString(146));
                     os.write((byte)(70));
                     os.write(toBytes(Integer.parseInt(tokens[1])));
                     os.write((byte)(146));
-                    //os.write(toBytes(Integer.parseInt(tokens[1])));
                 }
                 else if(tokens[0].equals("printv")){
                     //do printv stuff
+                    os.write((byte)(70));
+                    os.write(toBytes(symbol_table.get(tokens[1])));
+                    os.write((byte)(74));
+                    os.write((byte)(146));
                 }
                 else if(tokens[0].equals("jmp")){
                     //do jmp stuff
@@ -68,7 +62,6 @@ public class Compiler {
                 } else if (tokens[0].equals("cmpe")) {
                     //do cmpe stuff
                     os.write((byte)(132));
-
                 }
                 else if(tokens[0].equals("cmplt")){
                     //do cmplt stuff
@@ -80,11 +73,15 @@ public class Compiler {
                 }
                 else if(tokens[0].equals("pushi")){
                     //do pushi stuff
-                    //writer.write("70 " + tokens[1] + " 146");
+                    os.write((byte)(70));
+                    os.write(toBytes(Integer.parseInt(tokens[1])));
+                    stack.add(Integer.parseInt(tokens[1]));
                 }
                 else if(tokens[0].equals("pushv")){
                     //do pushv stuff
-
+                    os.write((byte)(70));
+                    os.write(toBytes(symbol_table.get(tokens[1])));
+                    os.write((byte)(74));
                 }
                 else if(tokens[0].equals("popm")){
                     //do popm stuff
@@ -94,12 +91,26 @@ public class Compiler {
                 }
                 else if(tokens[0].equals("popv")){
                     //do popv stuff
+                    os.write((byte)(70));
+                    os.write(toBytes(symbol_table.get(tokens[1])));
+                    os.write((byte)(80));
                 }
                 else if(tokens[0].equals("peek")){
                     //do peek stuff
+                    os.write((byte)(70));
+                    os.write(toBytes(symbol_table.get(tokens[1])));
+                    os.write((byte)(70));
+                    os.write(toBytes(Integer.parseInt(tokens[2])));
+                    os.write((byte)(86));
+
                 }
                 else if(tokens[0].equals("poke")){
                     //do poke stuff
+                    os.write((byte)(70));
+                    os.write(toBytes(symbol_table.get(tokens[2])));
+                    os.write((byte)(70));
+                    os.write(toBytes(Integer.parseInt(tokens[1])));
+                    os.write((byte)(90));
                 }
                 else if(tokens[0].equals("swp")){
                     //do swp stuff
@@ -128,10 +139,11 @@ public class Compiler {
                     os.write((byte)(48));
                 }
             }
-            //writer.close();
+            reader.close();
         }catch(Exception e) {
             System.out.println("Exception thrown when reading file");
         }
+        os.close();
     }
 
     public static byte[] toBytes(int i) {
